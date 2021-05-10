7d6f0b35fba92cb1b5f801e066f67fe69736b1c7
Samuel Howsden <samuel.howsden@student.centralia.edu>
2021-05-09 18:25:36 -0700

    Add project files.

---
+++ Howsden_Dialer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Howsden_Dialer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
